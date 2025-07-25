2fb5622b6404de7a2b00f67550f44f12a7c6ed3e
harish lodh <harishlodh444@gmail.com>
2025-07-25 17:34:34 +0530

    changes instance

--- src/api's/axiosInstance.ts
+++ src/api's/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080', // fallback if env missing
+  baseURL: import.meta.env.BACKEND_URL || 'http://localhost:8080', // fallback if env missing
   headers: {
     'Content-Type': 'application/json',
   },
